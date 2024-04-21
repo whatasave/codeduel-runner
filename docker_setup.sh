c177980921fafbc97dc7302372e9c90e68afecc3
Kaj Rusilowski <xedom08@gmail.com>
2024-04-21 17:46:00 +0200

    remove output when building lang containers

--- docker_setup.sh
+++ docker_setup.sh
@@ -14,7 +14,7 @@ find docker -mindepth 1 -maxdepth 1 -type d ! -name "_base" | while read -r dir;
   name=$(basename "$dir")
   cd "./docker/$name"
   cp -r ../_base ./base
-  docker build -t "${DOCKER_IMAGE_PREFIX}${name}" -f Dockerfile .
+  docker build -t "${DOCKER_IMAGE_PREFIX}${name}" -q -f Dockerfile .
   cd "../.."
   echo "$name" >> languages.txt
 done
