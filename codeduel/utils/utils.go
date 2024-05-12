d97c9b1cbca7e95bbeeeca6b81f367a38e60a306
Kaj Rusilowski <xedom08@gmail.com>
2024-05-12 11:13:00 +0200

    removed unused function

--- codeduel/utils/utils.go
+++ codeduel/utils/utils.go
@@ -3,16 +3,13 @@ package utils
 import (
 	"fmt"
 	"os"
-	"time"
 )
 
 func GetEnv(key string) (string, error) {
 	value, exists := os.LookupEnv(key)
-	if !exists { return "", fmt.Errorf("%s not defined in .env file", key) }
+	if !exists {
+		return "", fmt.Errorf("%s not defined in .env file", key)
+	}
 
 	return value, nil
-}
-
-func UnixTimeToTime(unixTime int64) time.Time {
-	return time.Unix(unixTime, 0)
 }
