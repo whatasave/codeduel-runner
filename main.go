7702362fdb1ee51f3fb9ac4f3c4b99ef56b7bf91
Kaj Rusilowski <xedom08@gmail.com>
2026-04-28 23:35:06 +0200

    remove unused code

--- main.go
+++ main.go
@@ -3,11 +3,8 @@ package main
 import (
 	_ "embed"
 	"log"
-	"os"
-	"path/filepath"
 
 	"github.com/docker/docker/client"
-	"github.com/joho/godotenv"
 	"github.com/xedom/codeduel/codeduel/api"
 	"github.com/xedom/codeduel/codeduel/discovery"
 	"github.com/xedom/codeduel/codeduel/runner"
@@ -46,51 +43,4 @@ func main() {
 	}
 
 	server.Run()
-}
-
-func loadingEnvVars() {
-	isProduction := os.Getenv("GO_ENV") == "production"
-	if isProduction {
-		return
-	}
-
-	wd, err := os.Getwd()
-	if err != nil {
-		log.Printf("[MAIN] Error getting working directory: %v", err)
-	}
-	pathDir, err := filepath.Abs(wd)
-	if err != nil {
-		log.Printf("[MAIN] Error getting absolute path: %v", err)
-	}
-
-	log.Printf("Loading .env file from %s", pathDir)
-	envPath := filepath.Join(pathDir, ".env")
-	if _, err := os.Stat(envPath); os.IsNotExist(err) {
-		log.Printf("[MAIN] Error: .env file not found in %s", pathDir)
-		return
-	}
-	err = godotenv.Load(envPath)
-	if err != nil {
-		log.Printf("[MAIN] Error loading .env file")
-	}
-}
-
-func warnUndefinedEnvVars() {
-	envVars := []string{
-		"HOST",
-		"PORT",
-		"DOCKER_IMAGE_PREFIX",
-		"DOCKER_TIMEOUT",
-	}
-
-	for _, envVar := range envVars {
-		test, exists := os.LookupEnv(envVar)
-		if !exists {
-			log.Printf("[MAIN] Warning: %s not defined in .env file", envVar)
-		}
-		if test == "" {
-			log.Printf("[MAIN] Warning: %s is empty", envVar)
-		}
-		log.Printf("[MAIN] %s: %s", envVar, test)
-	}
 }
